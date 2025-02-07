9f6ec6f531e201a72137d886adf6ab02373e0228
Teddy <chuangtheodore@gmail.com>
2025-02-07 21:51:29 +1100

    feat: Add state types

---
+++ src/types/states.ts
@@ -0,0 +1,22 @@
+import { JsonValue } from "@prisma/client/runtime/library";
+
+export enum ConversationState {
+    INITIAL_DISCOVERY = 'initial_discovery',
+    GOAL_SETTING = 'goal_setting',
+    ACTION_PLANNING = 'action_planning',
+    ACTIVE_COACHING = 'active_coaching',
+    PROGRESS_REVIEW = 'progress_review'
+  }
+  
+  export interface StateContext {
+    userId: string;
+    chatId: number;
+    currentState: ConversationState;
+    stateData?: JsonValue;
+  }
+  
+  export interface StateTransitionResult {
+    nextState: ConversationState;
+    response: string;
+    stateData?: Record<string, any>;
+  }
